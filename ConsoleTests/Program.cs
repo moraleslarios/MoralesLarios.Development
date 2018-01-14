82bf928f4ab33bcdf9811d06c9b631d12422906f
Juan Francisco Morales Larios <pakooo@hotmail.com>
2018-01-14 13:22:38 +0100

    tab code

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -12,65 +12,18 @@ namespace ConsoleTests
         static void Main(string[] args)
         {
 
-
             string numericString = Config.ConfigNodes.PropNumericForceString;
-
             string p1 = Config.ConfigNodes.PropertyForceString1;
-
-            string     p11  = Config.ConfigNodes.Propertytring1;
-
-
-
+            string p11 = Config.ConfigNodes.Propertytring1;
             decimal p2 = Config.ConfigNodes.PropertyNumeric1;
-
-
-
-
-            string[]   as1  = Config.ConfigNodes.PropertyArrayString1;
-
-
-
-
-
-            decimal[]  ad1  = Config.ConfigNodes.PropertyArrayNumeric;
-
-
-
-
-
-
-
-
-
-
-            DateTime   p3   = Config.ConfigNodes.PropertyDate;
-
-
-
-
-            DateTime   p4   = Config.ConfigNodes.PropertyDateTime;
-
-
-
-
+            string[] as1 = Config.ConfigNodes.PropertyArrayString1;
+            decimal[] ad1 = Config.ConfigNodes.PropertyArrayNumeric;
+            DateTime p3 = Config.ConfigNodes.PropertyDate;
+            DateTime p4 = Config.ConfigNodes.PropertyDateTime;
             DateTime[] adt1 = Config.ConfigNodes.PropertyArrayDate;
-
-
-
-            bool       p5   = Config.ConfigNodes.PropertyBool;
-
-
-
-            bool[]     ab1  = Config.ConfigNodes.PropertyArrayBool;
-
-
-
-
-            string     psch = Config.ConfigNodes.PropSaveChararcter;
-
-
-
-
+            bool p5 = Config.ConfigNodes.PropertyBool;
+            bool[] ab1 = Config.ConfigNodes.PropertyArrayBool;
+            string psch = Config.ConfigNodes.PropSaveChararcter;
             string data = Config.ConfigNodes.PropStringSaveComilla;
 
 
